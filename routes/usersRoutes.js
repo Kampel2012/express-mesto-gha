d126d2959881aca7549124ee5c7804abffee1569
Anton Glazunov <kampel2012@yandex.ru>
2023-06-29 18:23:05 +0500

    fix опечатка в роутере Joi

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -28,7 +28,7 @@ router.get(
   '/:userId',
   celebrate({
     params: Joi.object().keys({
-      _id: Joi.string().alphanum().length(24).hex(),
+      userId: Joi.string().alphanum().length(24).hex(),
     }),
   }),
   getUserById
