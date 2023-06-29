69d9a6345eea0b524cc84af50254ccf2c14b3175
Anton Glazunov <kampel2012@yandex.ru>
2023-06-29 18:14:58 +0500

    fix

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,36 @@
 import { Router } from 'express';
+import { celebrate, Joi } from 'celebrate';
 import { login, addNewUser } from '../controllers/usersControllers.js';
 import auth from '../middlewares/auth.js';
 import userRouter from './usersRoutes.js';
 import cardRouter from './cardsRoutes.js';
+import { validationRegex } from '../utils/constants.js';
 
 const router = Router();
 
-router.use('/signin', login);
-router.use('/signup', addNewUser);
+router.use(
+  '/signin',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+    }),
+  }),
+  login
+);
+router.use(
+  '/signup',
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(20),
+      avatar: Joi.string().regex(validationRegex),
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+    }),
+  }),
+  addNewUser
+);
 
 router.use('/users', auth, userRouter);
 router.use('/cards', auth, cardRouter);
