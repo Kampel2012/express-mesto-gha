930033864bcf1a24d6c9eaf9d8d02dda0125e5cb
Anton Glazunov <kampel2012@yandex.ru>
2023-06-30 16:00:59 +0500

    fix validation

--- routes/cardsRoutes.js
+++ routes/cardsRoutes.js
@@ -16,6 +16,9 @@ router.get('/', getAllCards);
 router.post(
   '/',
   celebrate({
+    params: Joi.object().keys({
+      _id: Joi.string().length(24).hex(),
+    }),
     body: Joi.object().keys({
       name: Joi.string().min(2).max(30).required(),
       link: Joi.string().required().regex(validationRegex),
