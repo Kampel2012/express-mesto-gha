985b510d562cf4b3923498dc0fd6d9d96ee4ee8d
Anton Glazunov <kampel2012@yandex.ru>
2023-07-11 14:27:36 +0500

    linter fix

--- app.js
+++ app.js
@@ -5,10 +5,17 @@ import mongoose from 'mongoose';
 import { errors } from 'celebrate';
 import routes from './routes/index.js';
 import { NotFoundError } from './errors/errors.js';
-
 import handleError from './middlewares/handeError.js';
+import { requestLogger, errorLogger } from './middlewares/logger.js';
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
 
 const app = express();
 
@@ -18,22 +25,19 @@ mongoose.connect(DB_URL, {
 
 app.use(helmet());
 
-const limiter = rateLimit({
-  windowMs: 10 * 60 * 1000,
-  max: 100,
-  standardHeaders: true,
-  legacyHeaders: false,
-});
-
 app.use(limiter);
 
 app.use(express.json());
+
+app.use(requestLogger);
 
 app.use(routes);
 
 app.use('*', () => {
   throw new NotFoundError('Данная страница не найдена');
 });
+
+app.use(errorLogger);
 
 app.use(errors());
 app.use(handleError);
